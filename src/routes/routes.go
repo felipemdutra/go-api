ee05f8369eaf1beedc6ed03dd2e89576192b036e
Felipe Dutra <felipemd.dutra@gmail.com>
2025-01-20 20:15:17 -0300

    remove unnecessary 'RouteHandler' func Renamed 'homePageRoute' to 'HomeRouteHandler'

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -1,32 +1,10 @@
 package routes
 
 import (
-	"log"
 	"net/http"
-	"os"
 )
 
-func RouteHandler() {
-    http.HandleFunc("/", homePageRoute)
-}
-
-func homePageRoute(w http.ResponseWriter, r *http.Request) {
-    // http.Header.Set()
-    file, err := os.Open("./src/html/index.html")
-    if err != nil {
-        log.Fatal(err)
-        return
-    }
-
-    defer file.Close()
-
-    fileInfo, err := file.Stat()
-
-    if err != nil {
-        log.Fatal(err)
-        return
-    }
-
-    http.ServeContent(w, r, "../html/index.html", fileInfo.ModTime(), file)
+func HomeRouteHandler(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "./src/html/index.html")
 }
 
