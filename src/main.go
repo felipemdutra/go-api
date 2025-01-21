7716a22b133939865dec33f4aa95a9017c8ac788
Felipe Dutra <felipemd.dutra@gmail.com>
2025-01-21 18:25:36 -0300

    feat: add another GET route to test server's router

--- src/main.go
+++ src/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"go-api/src/core"
 	"go-api/src/routes"
+	"net/http"
 )
-
 
 func main() {
     const PORT int = 8080
@@ -12,6 +13,10 @@ func main() {
     app := core.CreateServer(PORT)
 
     app.Get("/", routes.HomeRouteHandler)
+    app.Get("/test", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprint(w, "This is working!!!\n");
+    })
+
     app.Start()
 }
 
