ffb32c662641ac775d5d38774985606fb13996c0
Felipe Dutra <felipemd.dutra@gmail.com>
2025-01-21 22:04:57 -0300

    feat: add simple post handling function that prints information to the console and sends to the user

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -1,10 +1,32 @@
 package routes
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 )
 
 func HomeRouteHandler(w http.ResponseWriter, r *http.Request) {
     http.ServeFile(w, r, "./src/html/index.html")
+}
+
+func GetJsonResponse(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "User created!")
+    body, err := io.ReadAll(r.Body)
+    if err != nil {
+        http.Error(w, "Unable to read request body", http.StatusBadRequest)
+        return
+    }
+
+    var data map[string]interface{}
+    err = json.Unmarshal(body, &data)
+    if err != nil {
+        http.Error(w, "Invalid JSON", http.StatusBadRequest)
+        return
+    }
+
+    fmt.Fprintf(w, "User:\n%+v\n", data)
+    fmt.Printf("User:\n%+v\n", data)
 }
 
